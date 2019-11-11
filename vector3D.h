3dfc1c3add06d8e115af185700a477a70d81d658
Marina Filina <katerain45@gmail.com>
2019-11-11 11:17:42 +0300

    Update vector3D.h

--- vector3D.h
+++ vector3D.h
@@ -1,41 +1,68 @@
+#pragma once
 #include "stdafx.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
 
-    class vector3D
-    {
+class vector3D
+{
 
-	    public: vector3D() {
-		    a = 0;
-		    b = 0;
-		    c = 0;
-	    }
-	    
-	    private: 
-		int a;
-		int b;
-		int c;
+private:
+	int a;
+	int b;
+	int c;
 
-		int getA() { return a };
-		int getB() { return b };
-		int getC() { return c };
+public: vector3D() { //по умолчанию
+	a = 0;
+	b = 0;
+	c = 0;
+}
+
+public: vector3D(int _a, int _b, int _c = 2) { //с параметрами
+	c = _c;
+	a = _a;
+	b = _b;
+}
 
 
-		void setVector() 
+public: void inputVector()
+	{
+	
+		cout << "Введите целое число а: ";
+		cin >> a;
+		cout << "Введите целое число b: ";
+		cin >> b;
+		cout << "Введите целое число c: ";
+		cin >> c;
+	}
+
+	void outputVector()
+	{
+		cout << "a = " << a << endl;
+		cout << "b = " << b << endl;
+		cout << "c = " << c << endl;
+	}
+
+public:	int getA() { return a; }
+		int getB() { return b; }
+		int getC() { return c; }
+
+public: void setA(int _a)
 		{
-			cout << "Введите целое число а: ";
-			cin >> a;
-			cout << "Введите целое число b: ";
-			cin >> b;
-			cout << "Введите целое число c: ";
-			cin >> c;
+			a = _a;
+		}
+		void setB(int _b)
+		{
+			b = _b;
+		}
+		void setC(int _c)
+		{
+			c = _c;
 		}
 
-		void getVector() 
-		{
-			cout << "a = " << a << endl;
-			cout << "b = " << b << endl;
-			cout << "c = " << c << endl;
-		}
 
-    }
+		
+
+
+
+};
